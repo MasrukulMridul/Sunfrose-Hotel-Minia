0e36b453a6a225d0e7dbf08d435e90f76b3c9804
md umar faruk <mdumar11233>
2021-06-05 12:21:21 +0600

    added new code

--- src/consts/hotels.js
+++ src/consts/hotels.js
@@ -5,7 +5,7 @@ const hotels = [
     location: 'Green street,Central district',
     price: 120,
     image: require('../assets/hotel1.jpg'),
-    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet`,
   },
   {
     id: '2',
@@ -13,7 +13,7 @@ const hotels = [
     location: 'Yuki street',
     price: 70,
     image: require('../assets/hotel2.jpg'),
-    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet`,
   },
   {
     id: '3',
